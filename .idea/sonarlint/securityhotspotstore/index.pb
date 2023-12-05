69ac628390c5bf80a86538696460e6335c8189bf
Artem_Karaskevich <hitory@yahoo.com>
2023-12-05 16:12:39 +0300

    lecture_1 example_1

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@ S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+_
+/src/main/java/lecture_1_Java_basic/Example.java,f\d\fd3eb9003ac1c10dd74e220f44693c84f63b166f
---
+++ src/main/java/lecture_1_Java_basic/Example.java
@@ -0,0 +1,12 @@
+package lecture_1_Java_basic;
+
+/*
+    Это пример простой программы на Java
+*/
+public class Example {
+    //  Выполнение любой программы на Java начинается
+    //  с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Java правит Интернетом!");
+    }
+}
